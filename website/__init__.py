9381c103d41e19bdb7262c03f17a4d86f1106b95
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-09-19 19:35:37 +0000

    Primo Commit

---
+++ website/__init__.py
@@ -0,0 +1,25 @@
+# Questo file py rende il folder 'website' un package
+# Creazione l'app Flask
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'coseacaso'
+
+    from .views import views
+    from .auth import auth
+
+    from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'coseacaso'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
+
+    return app
