4316e906e438f217a5d3fede02b6715f7b0f0702
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-09-20 15:26:10 +0000

    Login - Sign Up Html

--- website/__init__.py
+++ website/__init__.py
@@ -10,15 +10,6 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'coseacaso'
-
-    from .views import views
-    from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='')
 
